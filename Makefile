ce1ad69ce458c533f3cb383aeb0707b331b370bb
RndmNode <whitleyzeller@gmail.com>
2024-02-13 18:47:14 -0700

    Initial commit

---
+++ Makefile
@@ -0,0 +1,11 @@
+CC = /usr/bin/clang++
+CFLAGS = -std=c++17 -fcolor-diagnostics -Wall -fansi-escape-codes -g -I./include -L./lib -Wno-deprecated
+LIBS = -framework OpenGL -framework Cocoa -framework IOKit -framework CoreVideo -framework CoreFoundation
+SRC = $(wildcard ./src/*.cpp) $(wildcard ./src/*.c) ./lib/libglfw.3.3.dylib ./lib/libGLEW.2.2.0.dylib
+OUT = ./bin/app
+
+all:
+	$(CC) $(CFLAGS) $(SRC) -o $(OUT) $(LIBS)
+
+clean:
+	rm -rf $(OUT).dSYM
---
+++ src/main.cpp
@@ -0,0 +1,331 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <iomanip>
+
+#define ASSERT(x) if (!(x)) __builtin_debugtrap(); // kill program
+#define GLCall(x) GLClearError();\
+    x;\
+    ASSERT(GLLogCall(#x, __FILE__, __LINE__)) // check for errors on openGL calls
+
+// Error Handling
+// --------------
+// clear errors
+static void GLClearError()
+{
+    while (glGetError() != GL_NO_ERROR);
+}
+
+// Convert GLenum error code to string
+char const* gl_error_string(GLenum const err) noexcept
+{
+  switch (err)
+  {
+    // opengl 2 errors (8)
+    case GL_NO_ERROR:
+      return "GL_NO_ERROR";
+
+    case GL_INVALID_ENUM:
+      return "GL_INVALID_ENUM";
+
+    case GL_INVALID_VALUE:
+      return "GL_INVALID_VALUE";
+
+    case GL_INVALID_OPERATION:
+      return "GL_INVALID_OPERATION";
+
+    case GL_STACK_OVERFLOW:
+      return "GL_STACK_OVERFLOW";
+
+    case GL_STACK_UNDERFLOW:
+      return "GL_STACK_UNDERFLOW";
+
+    case GL_OUT_OF_MEMORY:
+      return "GL_OUT_OF_MEMORY";
+
+    case GL_TABLE_TOO_LARGE:
+      return "GL_TABLE_TOO_LARGE";
+
+    // opengl 3 errors (1)
+    case GL_INVALID_FRAMEBUFFER_OPERATION:
+      return "GL_INVALID_FRAMEBUFFER_OPERATION";
+
+    // gles 2, 3 and gl 4 error are handled by the switch above
+    default:
+      ASSERT(!"unknown error");
+      return nullptr;
+  }
+}
+
+// Get errors on the function call
+static bool GLLogCall(const char* function, const char* file, int line)
+{
+    while (GLenum error = glGetError())
+    {
+        std::cout << "[OpenGL Error]: (" << gl_error_string(error) << "): " <<
+        function << " " << 
+        file << ": LINE " << line 
+        << std::endl;
+        return false;
+    }
+    return true;
+}
+
+// Resize window
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
+// Shader Source Struct
+struct ShaderProgramSource 
+{
+    std::string VertexSource;
+    std::string FragmentSource;
+};
+
+// Grab and parse shader code
+static ShaderProgramSource ParseShader(const std::string& filepath)
+{
+    std::ifstream stream(filepath);
+    if (!stream.is_open())
+        std::cout << "ERROR: Could not open shader file" << std::endl;
+
+    enum class ShaderType 
+    {
+        None = -1, VERTEX = 0, FRAGMENT = 1 
+    };
+
+    std::string line;
+    std::stringstream ss[2];
+    ShaderType type = ShaderType::None;
+    while (getline(stream, line))
+    {
+        if (line.find("#shader") != std::string::npos)
+        {
+            if (line.find("vertex") != std::string::npos)
+            {
+                type = ShaderType::VERTEX;
+            }
+            else if (line.find("fragment") != std::string::npos)
+            {
+                type = ShaderType::FRAGMENT;
+            }
+        } 
+        else 
+        {
+            ss[(int)type] << line << "\n";
+        }
+    }
+
+    stream.close();
+    return {ss[0].str(), ss[1].str()};
+}
+
+// Compile OpenGL Shader
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+    GLCall(unsigned int id = glCreateShader(type));
+    const char* src = source.c_str();
+    GLCall(glShaderSource(id, 1, &src, nullptr));
+    GLCall(glCompileShader(id));
+
+    int result;
+    GLCall(glGetShaderiv(id, GL_COMPILE_STATUS, &result));
+    if (result == GL_FALSE)
+    {
+        int length;
+        GLCall(glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length));
+        char* message = (char*) alloca(length * sizeof(char));
+        GLCall(glGetShaderInfoLog(id, length, &length, message));
+        std::cout << "Failed to compile " << 
+            (type == GL_VERTEX_SHADER ? "vertex" : "fragment") <<
+            " Shader:" << std::endl;
+        std::cout << message << std::endl;
+        GLCall(glDeleteShader(id));
+        return 0;
+    }
+
+    return id;
+}
+
+// Create OpenGL Shader
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+    unsigned int program = glCreateProgram();
+    GLCall(unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader));
+    GLCall(unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader));
+
+    GLCall(glAttachShader(program, vs));
+    GLCall(glAttachShader(program, fs));
+    GLCall(glLinkProgram(program));
+    GLCall(glValidateProgram(program));
+
+    // int success;
+    // char infoLog[512];
+    // glGetProgramiv(program, GL_LINK_STATUS, &success);
+    // if(!success) {
+    //     std::cout << "Error on linking shader program" << std::endl;
+    //     glGetProgramInfoLog(program, 512, NULL, infoLog);
+    //     std::cout << infoLog << std::endl;
+    // }
+
+    GLCall(glDeleteShader(vs));
+    GLCall(glDeleteShader(fs));
+
+    return program;
+}
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    #ifdef __APPLE__
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    #endif
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Test", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    // sync refresh rate
+    // glfwSwapInterval(1);
+
+    // init GLEW
+    // ---------
+    if(glewInit()!= GLEW_OK)
+        std::cout << "Could not init GLEW" << std::endl;
+
+    // Triangle
+    // --------
+    float positions[] = {
+        -0.5f, -0.5f, // 0
+         0.5f, -0.5f, // 1
+         0.5f,  0.5f, // 2
+        -0.5f,  0.5f, // 3
+    };
+    unsigned int indicies[] = {
+        0, 1, 2,
+        2, 3, 0
+    };
+
+    // Buffers
+    // -------
+    // init buffer and array obj
+    unsigned int buffer, VAO;
+    // generate buffer and array obj
+    GLCall(glGenBuffers(1, &buffer));
+    GLCall(glGenVertexArrays(1, &VAO));
+    GLCall(glBindVertexArray(VAO));
+    // bind buffer and define how to read the buffer data
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
+    GLCall(glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions, GL_STATIC_DRAW));
+    // enable the attributes
+    GLCall(glEnableVertexAttribArray(0));
+    GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0));
+
+    // buffer for indicies to draw multiple triangles
+    unsigned int index_buffer_obj;
+    GLCall(glGenBuffers(1, &index_buffer_obj));
+    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer_obj));
+    GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indicies, GL_STATIC_DRAW));
+
+    // Shaders
+    // -------
+    // grab shaders from file, create shaders, and use them
+    ShaderProgramSource source = ParseShader("res/shaders/test.shader");
+    unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
+    GLCall(glUseProgram(shader));
+
+    // Grab uniform location and set the uniform
+    GLCall(int location = glGetUniformLocation(shader, "u_Color"));
+    ASSERT(location != -1);
+    GLCall(glUniform4f(location, 0.5f, 0.2f, 0.7f, 1.0f));
+
+    // reset buffers
+    GLCall(glBindVertexArray(0));
+    GLCall(glUseProgram(0));
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
+
+    float r = 0.0f;
+    float increment = 0.05f;
+    // render loop
+    // -----------
+    while (!glfwWindowShouldClose(window))
+    {
+        // input
+        // -----
+        processInput(window);
+
+        /* Render here */
+        GLCall(glClear(GL_COLOR_BUFFER_BIT));
+
+        // shader
+        GLCall(glUseProgram(shader));
+        GLCall(glUniform4f(location, r, 0.2f, 0.7f, 1.0f));
+
+        // vertex array
+        glBindVertexArray(VAO);
+        
+        // index buffer
+        GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer_obj));
+
+        // draw
+        GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);)
+
+        // change red value of color that is fed into the uniform 'u_Color'
+        if (r > 1.0f)
+            increment = -0.05f;
+        else if (r < 0.0f)
+            increment = 0.05f;
+
+        r += increment;
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        GLCall(glfwSwapBuffers(window));
+        GLCall(glfwPollEvents());
+    }
+
+    // optional: de-allocate all resources once they've outlived their purpose:
+    // ------------------------------------------------------------------------
+    GLCall(glDeleteVertexArrays(1, &VAO));
+    GLCall(glDeleteBuffers(1, &buffer));
+    GLCall(glDeleteProgram(shader));
+    GLCall(glfwTerminate());
+    return 0;
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    GLCall(glViewport(0, 0, width, height));
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
